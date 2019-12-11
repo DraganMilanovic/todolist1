640734b1f81ad3ddf910af7682293315c1e6b13b
Fakultet Organizacionih Nauka <student1@prod.fon.bg.ac.rs>
2019-12-11 11:23:33 +0100

    inicijalna podesavanja

--- todo-frontend (1)/todo-frontend/src/main.js
+++ todo-frontend (1)/todo-frontend/src/main.js
@@ -1,8 +1,6 @@
 import "@babel/polyfill";
 import "mutationobserver-shim";
 import Vue from "vue";
-import axios from 'axios';
-import VueAxios from 'vue-axios';
 import "./plugins/bootstrap-vue";
 import App from "./App.vue";
 import router from "./router";
@@ -10,11 +8,8 @@ import store from "./store";
 
 Vue.config.productionTip = false;
 
-Vue.use(VueAxios, axios);
-
 new Vue({
   router,
   store,
   render: h => h(App)
 }).$mount("#app");
-
--- todo-frontend (1)/todo-frontend/src/store/index.js
+++ todo-frontend (1)/todo-frontend/src/store/index.js
@@ -4,9 +4,7 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-    baseUrl: 'http://localhost:8080/todo/todo-backend'
-  },
+  state: {},
   mutations: {},
   actions: {},
   modules: {}
